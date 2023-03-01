7d91f17379578f90726b2170a24897827a0a8261
Rahaddin Asadzade <rahaddinasad@gmail.com>
2023-03-01 17:09:11 +0400

    https://medium.com/@KosteRico/spring-aop-in-2021-level-up-your-logging-8d1498242ba2

--- src/main/java/az/raheddin/springbootaop/controller/MyController.java
+++ src/main/java/az/raheddin/springbootaop/controller/MyController.java
@@ -3,27 +3,39 @@ package az.raheddin.springbootaop.controller;
 import az.raheddin.springbootaop.entity.User;
 import az.raheddin.springbootaop.service.MyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/aop")
 public class MyController {
 
     private final MyService myService;
 
+    //not work
     @PostMapping("/param")
-    public void msj(@RequestParam String msj){
-        System.out.println("Start:"+msj);
+    public void msj(@RequestParam String msj) {
+        System.out.println("Start:" + msj);
         myService.myMsj(msj);
     }
 
+    //not work
     @PostMapping("/body")
-    public void msjBody(@RequestBody User msj){
-        System.out.println("Start:"+msj);
+    public void msjBody(@RequestBody User msj) {
+        System.out.println("Start:" + msj);
         myService.myMsjBody(msj);
+    }
+
+    //work
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/hello"
+    )
+    public ResponseEntity<?> getHello(
+            @RequestParam("name") String username
+    ) {
+        return ResponseEntity.ok("Hello, " + username);
     }
 
 
