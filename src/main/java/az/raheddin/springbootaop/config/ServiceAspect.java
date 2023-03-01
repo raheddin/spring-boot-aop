61d7cd1bcaac2dcab8dd9f9a382c7fbbbd549a4c
Rahaddin Asadzade <rahaddinasad@gmail.com>
2023-03-01 22:45:27 +0400

    test

--- src/main/java/az/raheddin/springbootaop/config/ServiceAspect.java
+++ src/main/java/az/raheddin/springbootaop/config/ServiceAspect.java
@@ -1,20 +1,46 @@
 package az.raheddin.springbootaop.config;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Before;
+import az.raheddin.springbootaop.entity.User;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 
 @Aspect
 @Component
-public class ServiceAspect {
+public class ServiceAspect extends RequestBodyAdviceAdapter {
+    @Override
+    public boolean supports(MethodParameter methodParameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        System.out.println("supports");
+        return false;
+    }
 
-    @Before("execution(* az.raheddin.springbootaop.service.MyService.myMsj(..))")
+    @Override
+    public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) throws IOException {
+        System.out.println("beforeBodyRead");
+        return null;
+    }
+
+    @Override
+    public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        System.out.println("afterBodyRead");
+        return null;
+    }
+
+    @Override
+    public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        System.out.println("handleEmptyBody");
+        return null;
+    }
+
+    /* @Before("execution(* az.raheddin.springbootaop.service.MyService.myMsj(..))")
     public void msjSerBefore(JoinPoint joinPoint){
         System.out.println("Before msj:"+joinPoint.getArgs()[0]);
-        // System.out.println(joinPoint.getArgs()[1]); //java.lang.ArrayIndexOutOfBoundsException
-       // System.out.println("Before msj:");
     }
 
     @After("execution(* az.raheddin.springbootaop.service.*.*(..))")
@@ -25,5 +51,5 @@ public class ServiceAspect {
     @Before("execution(* az.raheddin.springbootaop.service.MyService.myMsjBody(..))")
     public void msjBodySerBefore(JoinPoint joinPoint){
         System.out.println("Before msj:"+joinPoint.getArgs()[0]);
-    }
+    }*/
 }
